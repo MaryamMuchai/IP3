537cc91292d6cea13bb0056ab82bf6a4a69ba246
Marya <maryammuchai@gmail.com>
2021-02-28 23:28:03 +0300

    linking the toggle element for design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+    $("#design-icon").click(function(){
+      $("#design-icon").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-icon").slideDown('1500');
+    });
+  });
 $(document).ready(function() {
     $("#development-icon").click(function() {
     $("#development-icon").slideDown('1400').hide('1000');
